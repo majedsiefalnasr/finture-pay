a37f3cf1e4190b88c1ce0500c38a03cb96f72b3b
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2025-12-11 14:08:02 +0200

    test(icons): add new icons to the home page for testing

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,5 +9,9 @@ definePageMeta({
     <h1>Home Page</h1>
     <!-- Page-specific content -->
     <u-btn color="primary" text="Vuetify Button"></u-btn>
+
+    <u-icon icon="line:homeLine" size="128" class="two-tones" />
+    <u-icon icon="d3:wallet" size="128" />
+    <u-icon icon="features:ai" size="128" />
   </u-container>
 </template>
