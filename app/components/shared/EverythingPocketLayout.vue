66822a4283f8bd965fe1a8517e5d426db421cc6c
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2026-01-04 11:56:55 +0200

    feat: update EverythingPocketLayout styles and add responsive design

--- app/components/shared/EverythingPocketLayout.vue
+++ app/components/shared/EverythingPocketLayout.vue
@@ -11,14 +11,14 @@ const features = [
 <template>
   <div class="hero-card">
     <u-container>
-      <div class="content d-flex flex-column ga-8">
-        <div class="text">
+      <div class="content d-flex justify-end align-end ga-8">
+        <div class="text d-flex flex-column ga-4 ga-md-6">
           <h3 class="title">Everything in Your Pocket</h3>
 
           <u-list bg-color="transparent">
-            <u-list-item v-for="(item, i) in features" :key="i">
+            <u-list-item v-for="(item, i) in features" :key="i" class="list-item">
               <template #prepend>
-                <u-icon icon="hugeicons:tick-02" color="Blue" size="36" />
+                <u-icon icon="hugeicons:tick-02" color="Blue" class="icon" />
               </template>
 
               <u-list-item-title class="text-h3 font-weight-medium">{{ item }}</u-list-item-title>
@@ -72,5 +72,59 @@ const features = [
 
 .hero-card .content > * {
   z-index: 1;
+}
+
+.hero-card .content > .text {
+  margin-inline-start: 60%;
+}
+
+.hero-card .content > .text .icon {
+  width: 36px;
+}
+
+.hero-card .content > .text .list-item {
+  min-height: unset;
+}
+
+@media (max-width: 991.98px) {
+  .hero-card .content > .text {
+    margin-inline-start: 40%;
+  }
+}
+
+@media (max-width: 767.98px) {
+  .hero-card .content {
+    padding-inline: 20px;
+    padding-block: 20px;
+  }
+
+  .hero-card .content:before {
+    background-image: url('/assets/images/everything-pocket/bg-mobile.png');
+  }
+
+  .hero-card .content > .text {
+    margin-inline-start: 0%;
+    margin-block-start: 60%;
+    width: 100%;
+  }
+
+  .hero-card .content > .text .title {
+    font-size: 30px !important;
+    line-height: 120% !important;
+  }
+
+  .hero-card .content > .text .text-h3 {
+    font-size: 16px !important;
+  }
+
+  .hero-card .content > .text .icon {
+    width: 15px;
+  }
+}
+
+@media (max-width: 575.98px) {
+  .hero-card .content > .text {
+    margin-block-start: 120%;
+  }
 }
 </style>
