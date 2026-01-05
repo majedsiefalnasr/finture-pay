0feb7576ef2ed9322c15368a77c00554cde88b09
Majed Sief Alnasr <majedsiefalnasr@outlook.com>
2026-01-05 10:43:50 +0200

    feat: add Navbar component to default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 // Kumbh sans font
 import '@fontsource-variable/kumbh-sans'
+import Navbar from '~/components/shared/Navbar.vue'
 
 // Layout-specific logic
 </script>
 
 <template>
   <u-app>
+    <!-- Navigation Bar -->
+    <Navbar />
+
     <!-- Main content of the application -->
     <u-main>
       <slot />
